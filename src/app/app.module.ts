ced6fda412250024295c51c4e327e0d311f64c92
Anshul Jain <anshul_jain@live.com>
2024-03-03 19:42:50 +0530

    Initial input Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FileUploaderComponent } from './file-uploader/file-uploader.component'; // Import the component
+import { FileUploaderComponent } from './file-uploader/file-uploader.component';
+import { InputPageComponent } from './input-page/input-page.component'; // Import the component
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploaderComponent, // Declare the component
+    FileUploaderComponent,
+    InputPageComponent, // Declare the component
   ],
   imports: [BrowserModule],
   providers: [],
