4e6fd76b9c439287c99b525db0197961a847cc39
Anshul Jain <anshul_jain@live.com>
2024-03-03 21:53:03 +0530

    updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
 import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 import { InputPageComponent } from './input-page/input-page.component';
 import { SharedDataService } from './shared-data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/file-uploader', pathMatch: 'full' },
+  { path: '', component: FileUploaderComponent, pathMatch: 'full' },
   { path: 'file-uploader', component: FileUploaderComponent },
   { path: 'input-page', component: InputPageComponent },
   // Add more routes as needed
@@ -20,7 +22,13 @@ const routes: Routes = [
     FileUploaderComponent,
     InputPageComponent, // Declare the component
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+  ],
   providers: [SharedDataService],
   bootstrap: [AppComponent],
 })
